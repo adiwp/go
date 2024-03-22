27dff2d5af23ea77fa56e323bff44bdf409cf845
Adi Wahyu Pribadi <adi.wahyu.p@gmail.com>
2024-03-22 14:04:18 +0700

    praktikum anonymous function dan closure

--- progo/main.go
+++ progo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"progo/ratarata" // Mengimpor package
 )
 
@@ -14,4 +15,48 @@ func main() {
 
 	// Menampilkan hasil
 	fmt.Println("Rata-rata nilai siswa:", rataRata)
+
+	/*******/
+	// anonymous function
+	/*******/
+	jariJari := 5.0
+
+	// Menghitung luas dan keliling dengan anonymous function
+	luas := func(r float64) float64 {
+		return math.Pi * r * r
+	}(jariJari)
+
+	keliling := func(r float64) float64 {
+		return 2 * math.Pi * r
+	}(jariJari)
+
+	// Menampilkan hasil
+	fmt.Println("Luas lingkaran:", luas)
+	fmt.Println("Keliling lingkaran:", keliling)
+
+	// closure
+	// Batas nilai untuk setiap huruf
+	batasNilai := map[string]int{
+		"A": 90,
+		"B": 80,
+		"C": 70,
+		"D": 60,
+		"E": 0,
+	}
+
+	// Closure untuk menentukan nilai huruf
+	getNilaiHuruf := func(nilaiUjian int) string {
+		for huruf, batas := range batasNilai {
+			if nilaiUjian >= batas {
+				return huruf
+			}
+		}
+		return "E"
+	}
+
+	// Menentukan nilai huruf untuk beberapa nilai ujian
+	nilaiUjian := []int{85, 75, 95, 55, 65}
+	for _, nilai := range nilaiUjian {
+		fmt.Println("Nilai ujian", nilai, ":", getNilaiHuruf(nilai))
+	}
 }
